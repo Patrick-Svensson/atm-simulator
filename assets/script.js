e394dc1b8f3848696cb9b1b4ec4b3de90476ddb7
Patrick Svensson <ps.digitalepost@gmail.com>
2021-05-01 19:03:05 +0200

    Add object in script.js

---
+++ assets/script.js
@@ -0,0 +1,10 @@
+const account = {
+    accountName: "Santa Claus",
+    balance: 100,
+    getBalance: function () {},
+    deposit: function () {},
+    withdrawal: function () {},
+    getAccountName: function () {},
+    accountError: function () {},
+    exitAccount: function () {},
+};
