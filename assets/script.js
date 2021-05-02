272206d1f417b8ea15aa5067ede87c12f6d4b3a3
Patrick Svensson <ps.digitalepost@gmail.com>
2021-05-02 11:16:35 +0200

    Declare variables.

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,10 @@
+btnBalance = document.querySelector(".btn__balance");
+btnDeposit = document.querySelector(".btn__deposit");
+btnWithdrawal = document.querySelector(".btn__withdrawal");
+btnAccountName = document.querySelector(".btn__accountName");
+btnExit = document.querySelector(".btn__exit");
+btnSubmit = document.querySelector(".btn__submit");
+
 const account = {
     accountName: "Santa Claus",
     balance: 100,
