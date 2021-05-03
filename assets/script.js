6d44438d98a0b41020b99fb1277fe08e51f7a1d9
Patrick Svensson <ps.digitalepost@gmail.com>
2021-05-03 19:55:01 +0200

    Error handeling.

--- assets/script.js
+++ assets/script.js
@@ -14,14 +14,14 @@ const account = {
     }),
 
     deposit: btnDeposit.addEventListener("click", () => {
-        document.querySelector(".message").innerHTML = "Type the amount";
+        document.querySelector(".message").innerHTML = "Please, enter an amount";
         document.querySelector(".input-field").value = 0;
         document.querySelector(".input-field").focus();
         document.querySelector(".input-field").select();
     }),
 
     withdrawal: btnWithdrawal.addEventListener("click", () => {
-        document.querySelector(".message").innerHTML = `(€3 withdrawal fee) Type the amount`;
+        document.querySelector(".message").innerHTML = "(€3 withdrawal fee) Please, enter an amount";
         document.querySelector(".input-field").value = 0;
         document.querySelector(".input-field").focus();
         document.querySelector(".input-field").select();
@@ -36,25 +36,33 @@ const account = {
     }),
 
     clearMessage: btnClear.addEventListener("click", () => {
-        document.querySelector(".message").innerHTML = `PRESS BUTTON TO CHOOSE ACTION`;
+        document.querySelector(".message").innerHTML = "PRESS BUTTON TO CHOOSE ACTION";
         document.querySelector(".input-field").value = 0;
     }),
 
     enterInput: enter.addEventListener("click", () => {
         let inputValue = parseInt(document.querySelector(".input-field").value);
 
-        if (document.querySelector(".message").innerHTML === `(€3 withdrawal fee) Type the amount` && inputValue > 0) {
+        if (
+            document.querySelector(".message").innerHTML === "(€3 withdrawal fee) Please, enter an amount" &&
+            inputValue > 10 &&
+            inputValue < account.balance
+        ) {
             account.balance -= inputValue + 3;
+            document.querySelector(".message").innerHTML = "The amount has been deducted from Your account.";
             document.querySelector(".input-field").value = 0;
-        } else if (document.querySelector(".message").innerHTML === "Type the amount") {
+        } else if (inputValue < 10) {
+            document.querySelector(".message").innerHTML = "Please, enter an amount rounded up to €10.";
+        } else if (inputValue > account.balance) {
+            document.querySelector(
+                ".message"
+            ).innerHTML = `Your balance is €${account.balance}. Please enter new amount.`;
+        } else if (document.querySelector(".message").innerHTML === "Please, enter an amount" && inputValue > 10) {
             account.balance += inputValue;
+            document.querySelector(".message").innerHTML = "The amount has been deposited to Your account.";
             document.querySelector(".input-field").value = 0;
         } else {
             return;
         }
     }),
-
-    accountError: function () {},
-    // if not a number
-    //
 };
