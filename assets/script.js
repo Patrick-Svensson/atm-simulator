ae9680d3f38f423184424d15e50756fc322a1ce2
Patrick Svensson <ps.digitalepost@gmail.com>
2021-05-03 14:28:52 +0200

    Add currency symbol.

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,7 @@
 btnBalance = document.querySelector(".btn__balance");
 btnDeposit = document.querySelector(".btn__deposit");
 btnWithdrawal = document.querySelector(".btn__withdrawal");
-btnAccountName = document.querySelector(".btn__accountName");
+btnAccountName = document.querySelector(".btn__account-name");
 btnExit = document.querySelector(".btn__exit");
 btnClear = document.querySelector(".btn__clear");
 btnSubmit = document.querySelector(".btn__submit");
@@ -12,14 +12,26 @@ const account = {
     getBalance: btnBalance.addEventListener("click", () => {
         document.querySelector(".message").innerHTML = `&#8364;${account.balance}`;
     }),
-    deposit: function () {},
-    withdrawal: function () {},
-    getAccountName: function () {},
+    deposit: btnDeposit.addEventListener("click", () => {
+        document.querySelector(".message").innerHTML = `Type the amount (round up to &#8364;10)`;
+        document.querySelector(".input-field").focus();
+        document.querySelector(".input-field").select();
+    }),
+    withdrawal: btnWithdrawal.addEventListener("click", () => {
+        document.querySelector(".message").innerHTML = `Type the amount (round up to &#8364;10)`;
+        document.querySelector(".input-field").focus();
+        document.querySelector(".input-field").select();
+        document.querySelector(".input-field").
+    }),
+    getAccountName: btnAccountName.addEventListener("click", () => {
+        document.querySelector(".message").innerHTML = `${account.accountName}`;
+    }),
     accountError: function () {},
     exitAccount: btnExit.addEventListener("click", () => {
         return window.location.assign("../../index.html");
     }),
     clearMessage: btnClear.addEventListener("click", () => {
         document.querySelector(".message").innerHTML = `PRESS BUTTON TO CHOOSE ACTION`;
+        document.querySelector(".input-field").value = null;
     }),
 };
