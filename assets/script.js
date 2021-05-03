be9b36dc393ffd5e91b224052a9ccd146487101f
Patrick Svensson <ps.digitalepost@gmail.com>
2021-05-03 18:15:09 +0200

    Add functions for diposit and withdrawal.

--- assets/script.js
+++ assets/script.js
@@ -4,24 +4,23 @@ btnWithdrawal = document.querySelector(".btn__withdrawal");
 btnAccountName = document.querySelector(".btn__account-name");
 btnExit = document.querySelector(".btn__exit");
 btnClear = document.querySelector(".btn__clear");
-btnSubmit = document.querySelector(".btn__submit");
+enter = document.querySelector(".btn__enter");
 
 const account = {
-    accountName: "Santa Claus",
+    accountName: "Jane Doe",
     balance: 100,
     getBalance: btnBalance.addEventListener("click", () => {
         document.querySelector(".message").innerHTML = `&#8364;${account.balance}`;
     }),
-    deposit: btnDeposit.addEventListener("click", () => {
-        document.querySelector(".message").innerHTML = `Type the amount (round up to &#8364;10)`;
+    deposit: btnDeposit.addEventListener("click", function () {
+        document.querySelector(".message").innerHTML = "Type the amount";
         document.querySelector(".input-field").focus();
         document.querySelector(".input-field").select();
     }),
     withdrawal: btnWithdrawal.addEventListener("click", () => {
-        document.querySelector(".message").innerHTML = `Type the amount (round up to &#8364;10)`;
+        document.querySelector(".message").innerHTML = `(€3 withdrawal fee) Type the amount`;
         document.querySelector(".input-field").focus();
         document.querySelector(".input-field").select();
-        document.querySelector(".input-field").
     }),
     getAccountName: btnAccountName.addEventListener("click", () => {
         document.querySelector(".message").innerHTML = `${account.accountName}`;
@@ -32,6 +31,20 @@ const account = {
     }),
     clearMessage: btnClear.addEventListener("click", () => {
         document.querySelector(".message").innerHTML = `PRESS BUTTON TO CHOOSE ACTION`;
-        document.querySelector(".input-field").value = null;
+        document.querySelector(".input-field").value = 0;
     }),
 };
+
+enter.addEventListener("click", () => {
+    let inputValue = parseInt(document.querySelector(".input-field").value);
+
+    if (document.querySelector(".message").innerHTML == `(€3 withdrawal fee) Type the amount`) {
+        account.balance -= inputValue + 3;
+        document.querySelector(".input-field").value = 0;
+    } else if (document.querySelector(".message").innerHTML === "Type the amount") {
+        account.balance += inputValue;
+        document.querySelector(".input-field").value = 0;
+    } else {
+        return;
+    }
+});
